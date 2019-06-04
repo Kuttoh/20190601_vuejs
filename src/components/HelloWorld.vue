0357446d4c4db549649ff8356ca91c697854043d
Kuttoh <kuttohisaac@gmail.com>
2019-06-04 23:23:30 +0300

    Add Templates, lists and class bindirng

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
 
     <input type="text" id="input" v-model="msg">
 
-<p>Welcome Home: {{msg}}</p>
+    <p>Welcome Home: {{msg}}</p>
 
     <ul>
         <li v-for="name in names" v-text="name"></li>
@@ -13,6 +13,10 @@
     <input id="input" type="text" v-model="newName">
 
     <button v-on:click="addName">Add Name</button>
+
+    <div>
+        <button :title="title">Hover over me</button>
+    </div>
 
 </div>
 
@@ -24,17 +28,17 @@
         data(){
             return {
                 msg: 'Lion King',
-                newName: '',
-                names: ['Sharon', "Sam", "PostMarlone", "Kuttoh" ]
+                newName: '',  //initialize variable newName
+                names: ['Sharon', "Sam", "PostMarlone", "Kuttoh" ],
+                title : "What are you looking for?"
             }
         },
 
         methods:{
             addName() {
-                this.names.push(this.newName) //add new name
-                this.newName =  '';
+                this.names.push(this.newName) //add new name to array
+                this.newName =  ''; //reset input field to blank
             }
-
         }
     }
 </script>
