7033e483428f141e986d34967d979b5a4d237604
Kuttoh <kuttohisaac@gmail.com>
2019-06-04 19:50:34 +0300

    first Vue Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div>
+
+    <input type="text" id="input" v-model="msg">
+
+<p>Welcome Home: {{msg}}</p>
+
+    <ul>
+        <li v-for="name in names" >{{name}}</li>
+    </ul>
+
+</div>
+
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default {
+        data(){
+            return {
+                msg: 'Lion King',
+                names: ['Sharon', "Sam", "PostMarlone", "Kuttoh" ]
+            }
+        }
+    }
+</script>
+
+<style>
+  input{
+    color:rgb(0,107,91);
+  }
+</style>
